e6611db73bbe6418d1bdda4ed28af12bfd70b8fb
IrvingHuang <S409896@sinyi.com.tw>
2019-08-26 11:07:51 +0800

    Initial Commit

---
+++ autoBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoBuild
+//
+//  Created by IrvingHuang on 2019/8/26.
+//  Copyright © 2019 Irving Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
